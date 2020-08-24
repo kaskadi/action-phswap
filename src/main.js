e9c9fa1513ac06966ef4f6a3dd361145e026baf9
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-24 13:29:13 +0200

    Using pre/main logic for action

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 if (!process.env.DUPLETS) {
   console.log('No environment variable found for DUPLETS, not proceeding to swap placeholders...')
 } else {
-  require('./helpers/install-dependencies.js')()
   const duplets = process.env.DUPLETS
   console.log(duplets)
 }
