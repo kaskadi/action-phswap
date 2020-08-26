0959f3fcbcc85cab6d863e763eaef55f77b3ba9b
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-26 12:31:27 +0200

    Added pre step testing

--- test/action.test.js
+++ test/action.test.js
@@ -1,20 +1,26 @@
 /* eslint-env mocha */
 const runAction = require('./helpers/run-action.js')
-const steps = ['pre', 'main']
 const chai = require('chai')
 const { assert } = require('chai')
 chai.should()
 
 describe('action-phswap', function () {
-  const exp = true
-  it('should process inputs', async function () {
-    process.env.DUPLETS = '[placeholder]:[value]'
-    await runAction(steps)
-    assert(exp === true)
-  })
-  it('should warn that there is no inputs if none found', async function () {
-    delete process.env.DUPLETS
-    await runAction(steps)
-    assert(exp === true)
+  // ******* DO NOT REMOVE THIS TEST!
+  require('./pre/tests.js')
+  // *******
+  describe('placeholder test', function () {
+    this.timeout(30000)
+    const steps = ['pre', 'main']
+    const exp = true
+    it('should process inputs', async function () {
+      process.env.DUPLETS = '[placeholder]:[value]'
+      await runAction(steps)
+      assert(exp === true)
+    })
+    it('should warn that there is no inputs if none found', async function () {
+      delete process.env.DUPLETS
+      await runAction(steps)
+      assert(exp === true)
+    })
   })
 })
